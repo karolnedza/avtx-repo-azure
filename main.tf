18873ee6dea3d476f6158871bd621a94242ecfff
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-28 20:00:41 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aviatrix" {
+  username     = "admin"
+  password      = var.example
+  controller_ip = "35.171.31.227"
+  version       = "2.17.0"
+}
+
+
+resource "aviatrix_segmentation_security_domain" "security_segment_domain" {
+  domain_name = var.security_domain_name
+}
