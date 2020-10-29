78db66154ed0d7a88400dc546fc841569fadbf13
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-29 13:24:40 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "example" {}            # This is CTRL Password
 variable "pet_name_length" {}    # This is Domain Name
 variable "vcs_repository" {}     
-variable "workspace_response" {}
+variable "aviatrix" {}
